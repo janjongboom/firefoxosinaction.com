68e2461d1d572c53591d59ea6819b3e7ce230b42
Jan Jongboom <janjongboom@gmail.com>
2014-07-30 09:47:45 +0000

    devicemotion basis

---
+++ devicemotion/basis/dm.js
@@ -0,0 +1,12 @@
+(function() {
+  if (!window.DeviceOrientationEvent) {
+    return console.error('No support for device orientation');
+  }
+
+  window.addEventListener('deviceorientation', function(ev) {
+    var tiltLR = ev.gamma;
+
+    document.querySelector('#land').style.transform =
+      'rotate(' + tiltLR + 'deg)';
+  });
+})();
